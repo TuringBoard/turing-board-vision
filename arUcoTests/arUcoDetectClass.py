312da77bfdf2546d5439da0ddcccdaf619d82c19
sahajamatya <sahajamatya1@gmail.com>
2022-04-25 16:44:21 -0500

    tested solenoid integration - unsuccessful

--- arUcoTests/arUcoDetectClass.py
+++ arUcoTests/arUcoDetectClass.py
@@ -12,13 +12,6 @@ sys.path.insert(0, '../../turing-board-controls/src/Communication')
 
 from uart0Communication import *
 from communicate import SerialCommunication
-'''
-[[ 14.   7.]
- [ 94.   7.]
- [100.  83.]
- [ 20.  84.]]
-'''
-
 
 def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -35,7 +28,6 @@ class FollowMe:
     def __init__(self, serial_port='/dev/ttyACM1'):
         self.turningMechanism = SerialCommunication(port=serial_port, baudrate=115200)
         self.dc = DepthCamera()
-        # cap = cv2.VideoCapture(3)
         # This file should really be passed in as an argument to the class parameters
         self.camera_matrix = np.loadtxt("cameraMatrix_webcam.txt", delimiter=',')
         self.camera_distortion = np.loadtxt(
@@ -81,13 +73,11 @@ class FollowMe:
                 height = int(abs(bottomLeft[1]-topLeft[1]))
                 x_mid = int(topLeft[0] + (width/2))
                 y_mid = int(topLeft[1] + (height/2))
-                # print(topLeft[1], width)
                 for i in range(int(topLeft[0]+(width/4)), int(topRight[0]-(width/4))):
                     for j in range(int(topLeft[1]+(height/4)), int(bottomLeft[1]-(height/4))):
                         distance = depth_frame[j, i]
                         if distance > 0: 
                             storage.append(distance)
-                # print(storage)
                 cv2.circle(img, (x_mid, y_mid), 10, (0,0,255))
                 cv2.circle(imgGray, (x_mid, y_mid), 4, (0,0,255))
                 cv2.line(imgGray, (x_mid, 0), (x_mid, 480), (255,255,255), 1)
@@ -110,9 +100,6 @@ class FollowMe:
                 cv2.line(imgGray, bottomRight, bottomLeft, (255, 255, 255), 4)
                 cv2.line(imgGray, bottomLeft, topLeft, (255, 255, 255), 4)
                     
-                # aruco.drawAxis(img, self.camera_matrix,
-                #                self.camera_distortion, rvec, tvec, 10)
-            
             if len(storage) > 0: 
                 distance = int(min(storage)) + 10
             else: 
@@ -122,36 +109,21 @@ class FollowMe:
                 move_callback(-duty_cycle)
                 cv2.putText(imgGray, 'MOVE FORWARD'.format(distance), (450, 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                
                 if x_mid < rectX:
                     cv2.putText(imgGray, f'KEEP LEFT', (450, 80),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), )
-                    self.updateAngle(20)
-
+                    self.updateAngle(20, 0)
                 elif x_mid > (rectX+deadZoneWidth):
                     cv2.putText(imgGray, f'KEEP RIGHT', (450, 80),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    self.updateAngle(80)
-
+                    self.updateAngle(80, 0)
                 else: 
-                    self.updateAngle(50)
-
+                    self.updateAngle(50, 0)
             else:
                 move_callback(0)
                 cv2.putText(imgGray, 'STOPPED'.format(distance), (450, 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                # Controls the turning mechanism
-            
-
-    
-        # if len(corners) <= 0:
-        #     move_callback(0)
-        # cv2.putText(imgGray, f'TODO: DEPTH CALCULATION FOR FORWARD PROPULSION',
-        #             (900, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-        # cv2.namedWindow("BGR", cv2.WINDOW_NORMAL)
         cv2.namedWindow("arUco", cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow('BGR', 640, 480)
         cv2.resizeWindow('arUco', 640, 480)
         cv2.imshow("arUco", imgGray)
         self.key = cv2.waitKey(1)
@@ -159,57 +131,21 @@ class FollowMe:
     def close_follow_me(self):
         cv2.destroyAllWindows()
 
-    def updateAngle(self, angle1): 
-        # print("update command sent", angle1)
-
-        data = []
-        data.append(1 & 0xFF)  
-        data.append(int(angle1) & 0xFF)
-        direction = 2
-        if angle1 > 50:
-            direction = 1
-        elif angle1 < 50: 
-            direction = 0
-        if angle1 == 50: 
-            direction = 2
-        data.append(int(direction) & 0xFF)
-        rate = 0
-        data.append(int(rate) & 0xFF)
-        if angle1 != self.previous:
-            # print("data:",data)
+    def updateAngle(self, angle, lock):
+        id = 3 & 0xFF
+        angle = int(angle) & 0xFF
+        direction = 2 & 0xFF
+        if angle > 50:
+            direction = 1 & 0xFF
+        elif angle < 50: 
+            direction = 0 & 0xFF
+        lock = lock & 0xFF
+        data = [id, angle, direction, lock]
+        if angle != self.previous:
+            print(data)
             toSend = bytearray(data)
-            # print("bytearray:", toSend)
             self.turningMechanism.push(toSend)
-            # print("b4", toSend)
             self.turningMechanism.send()
-            # print("after")
-        self.previous = angle1
+            print(toSend)
+        self.previous = angle
 
-"""
-                # cv2.putText(imgGray, 'Unoptimized Distance: {}mm'.format(depth_frame[y_mid, x_mid]), (450, 140),
-                #                     cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 255, 255), 1)
-                # if True:
-                    # Controls the duty cycle of the wheels.
-                    # If the distance happens to be greater the given distance, move the wheels forward,
-                    # otherwise, stop!
-
-                if distance > distance_until_follow_me_on:
-                    move_callback(-duty_cycle)
-                    cv2.putText(imgGray, 'MOVE FORWARD'.format(distance), (450, 60),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                else:
-                    move_callback(0)
-                    cv2.putText(imgGray, 'STOPPED'.format(distance), (450, 60),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    printName("import successful")
-                    updateAngle(20)
-                    
-                # Controls the turning mechanism
-                if x_mid < rectX:
-                    cv2.putText(imgGray, f'KEEP LEFT', (450, 80),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), )
-                    
-                elif x_mid > (rectX+deadZoneWidth):
-                    cv2.putText(imgGray, f'KEEP RIGHT', (450, 80),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-"""
